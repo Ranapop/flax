dd6bc051cc5148a3eda6ddd0a737bbbad2cc539d
ranapop <roxanapop@google.com>
2020-09-17 17:10:21 +0000

    Add multilayer LSTM for decoder + dropout on encoder and decoder

--- examples/cfq/models.py
+++ examples/cfq/models.py
@@ -14,9 +14,10 @@
 
 # Lint as: python3
 """Flax modules composing the seq2seq LSTM architecture for CFQ"""
-from typing import Tuple
+from typing import Tuple, List
 import numpy as np
 import jax
+from jax import random
 import jax.numpy as jnp
 from flax import jax_utils
 from flax import nn
@@ -25,7 +26,8 @@ from flax import nn
 LSTM_HIDDEN_SIZE = 512
 EMBEDDING_DIM = 200
 ATTENTION_SIZE = 100
-
+NUM_LAYERS = 2
+DROPOUT = 0.4
 
 class Encoder(nn.Module):
   """LSTM encoder, returning state after EOS is input."""
@@ -34,12 +36,19 @@ class Encoder(nn.Module):
             inputs: jnp.array,
             lengths: jnp.array,
             shared_embedding: nn.Module,
-            hidden_size: int = LSTM_HIDDEN_SIZE):
+            train: bool,
+            hidden_size: int,
+            num_layers: int,
+            horizontal_dropout_rate: float,
+            vertical_dropout_rate: float):
 
     inputs = shared_embedding(inputs)
     lstm = nn.LSTM.partial(hidden_size=hidden_size,
-                           num_layers=1).shared(name='lstm')
-    outputs, final_states = lstm(inputs, lengths)
+                           num_layers=num_layers,
+                           dropout_rate=vertical_dropout_rate,
+                           recurrent_dropout_rate=horizontal_dropout_rate
+                           ).shared(name='lstm')
+    outputs, final_states = lstm(inputs, lengths, train = train)
     return outputs, final_states[-1]
 
 
@@ -54,7 +63,7 @@ class MlpAttention(nn.Module):
             hidden_size: int = None) -> jnp.ndarray:
     """Computes MLP-based attention based on keys and a query.
 
-    Attention scores are computed by feeding the keys and query through an MLP. 
+    Attention scores are computed by feeding the keys and query through an MLP.
     This results in a single scalar per key, and for each sequence the attention
     scores are normalized using a softmax so that they sum to 1. Invalid key
     positions are ignored as indicated by the mask. This is also called
@@ -91,8 +100,70 @@ class MlpAttention(nn.Module):
     return attention  # Shape: <float32>[batch_size, seq_len]
 
 
+class MultilayerLSTM(nn.Module):
+  "LSTM cell with multiple layers"
+
+  def apply(self,
+        num_layers,
+        horizontal_dropout_masks,
+        vertical_dropout_masks,
+        input: jnp.array,
+        previous_states: List):
+    """
+    Args
+      num_layers: number of layers
+      horizontal_dropout_masks: dropout masks for each layer (the same dropout
+        mask is used at each time step and applied on the hidden state that is
+        fed into the cell to the right) [num_layers, batch_size, hidden_size]
+      vertical_dropout_masks: dropout masks between layers, same masks reused
+        accross time steps, applied on the hidden state that is fed to the upper
+        layer (used only between layers, not on final output). The dropout is
+        the one used here https://arxiv.org/abs/1512.05287 except it's not
+        applied on the first layer inputs and final outputs to be consistent
+        with the implementation in `recurrent.py`
+        shape [num_layers-1, batch_size, hidden_size]
+      input: input given to the first LSTM layer [batch_size, input_size]
+      previous_states: list of (c,h) for each layer
+        shape [num_layers, batch_size, 2*hidden_size]
+    """
+    cell_input = input
+    states = []
+    final_output = None
+    for layer_idx in range(num_layers):
+      lstm_name = 'lstm_layer'+str(layer_idx)
+      cell = nn.LSTMCell.shared(name=lstm_name)
+      c,h = previous_states[layer_idx]
+      # apply dropout to h
+      if horizontal_dropout_masks[layer_idx] is not None:
+        h = h * horizontal_dropout_masks[layer_idx]
+      # apply dropout to hidden state from lower layer
+      if layer_idx!=0 and vertical_dropout_masks[layer_idx-1] is not None:
+        cell_input = cell_input * vertical_dropout_masks[layer_idx-1]
+      state, output = cell((c,h),cell_input)
+      states.append(state)
+      cell_input = output
+      final_output = output
+    return states, final_output
+
+
 class Decoder(nn.Module):
   """LSTM decoder."""
+
+  def create_dropout_masks(self,
+                           num_masks: int,
+                           shape: Tuple,
+                           dropout_rate: float):
+    if dropout_rate==0:
+      return [None] * num_masks
+    masks = []
+    for i in range(0, num_masks):
+      # should I use a different rng for each mask?
+      dropout_mask = random.bernoulli(nn.make_rng(),
+                                      p=1 - dropout_rate, shape=shape)
+      # Scale mask.
+      dropout_mask = dropout_mask / (1.0 - dropout_rate)
+      masks.append(dropout_mask)
+    return masks
 
   def apply(self,
             init_state,
@@ -101,8 +172,27 @@ class Decoder(nn.Module):
             inputs: jnp.array,
             shared_embedding: nn.Module,
             vocab_size: int,
-            teacher_force: bool = False):
-    lstm_cell = nn.LSTMCell.shared(name='lstm')
+            num_layers: int,
+            horizontal_dropout_rate: int,
+            vertical_dropout_rate: int,
+            train: bool = False):
+    """
+    Args
+      init_state: state to initialize the decoder hidden state (coming from the
+        encoder) [batch_size, hidden_size]
+      encoder_hidden_states: encoder hidden states
+        [batch_size, input_seq_len, hidden_size]
+      attention_mask: attention mask [batch_size, input_seq_len]
+      inputs: on the train flow (train=True) the gold decoded sequence (as
+        teacher forcing is used), on the inference flow an array of desired
+        output length where the first token is the BOS token
+      shared_embedding: module for computing the embeddings (shared with
+        the encoder)
+      vocab_size: vocabulary size
+      num_layers: number of layers in the LSTM
+      train: boolean choosing from train and inference flow
+    """
+    multilayer_lstm_cell = MultilayerLSTM.partial(num_layers = num_layers).shared(name='multilayer_lstm')
     projection = nn.Dense.shared(features=vocab_size, name='projection')
     mlp_attention = MlpAttention.partial(hidden_size=ATTENTION_SIZE).shared(
         name='attention')
@@ -111,24 +201,41 @@ class Decoder(nn.Module):
                               ATTENTION_SIZE,
                               name='keys',
                               bias=False)
+    
+    batch_size = encoder_hidden_states.shape[0]
+    hidden_size = encoder_hidden_states.shape[-1]
+    h_dropout_masks = self.create_dropout_masks(num_masks=num_layers,
+                                                shape=(batch_size, hidden_size),
+                                                dropout_rate=horizontal_dropout_rate)
+    v_dropout_masks = self.create_dropout_masks(num_masks=num_layers-1,
+                                                shape=(batch_size, hidden_size),
+                                                dropout_rate=vertical_dropout_rate)
 
     def decode_step_fn(carry, x):
-      rng, (c, h), last_prediction = carry
+      rng, multilayer_lstm_output, last_prediction = carry
+      previous_states, h = multilayer_lstm_output
       carry_rng, categorical_rng = jax.random.split(rng, 2)
-      if not teacher_force:
+      if not train:
         x = last_prediction
       x = shared_embedding(x)
       dec_prev_state = jnp.expand_dims(h, 1)
       attention = mlp_attention(dec_prev_state, projected_keys,
                                 encoder_hidden_states, attention_mask)
-      lstm_state, y = lstm_cell((c, attention), x)
+      previous_states[0] = (previous_states[0][0],attention)
+      
+      states, y = multilayer_lstm_cell(horizontal_dropout_masks=h_dropout_masks,
+                                       vertical_dropout_masks=v_dropout_masks,
+                                       input=x,
+                                       previous_states=previous_states)
       logits = projection(y)
       predicted_tokens = jax.random.categorical(categorical_rng, logits)
       predicted_tokens_uint8 = jnp.asarray(predicted_tokens, dtype=jnp.uint8)
-      return (carry_rng, lstm_state,
+      return (carry_rng, (states, y),
               predicted_tokens_uint8), (logits, predicted_tokens_uint8)
 
-    init_carry = (nn.make_rng(), init_state, inputs[:, 0])
+    init_states = [init_state] * num_layers
+    multilayer_lstm_output = (init_states, init_state[1])
+    init_carry = (nn.make_rng(), multilayer_lstm_output, inputs[:, 0])
 
     if self.is_initializing():
       # initialize parameters before scan
@@ -147,13 +254,18 @@ def compute_attention_masks(mask_shape: Tuple, lengths: jnp.array):
   mask = mask * (lengths[:, jnp.newaxis] > jnp.arange(mask.shape[1]))
   return mask
 
-
 class Seq2seq(nn.Module):
   """Sequence-to-sequence class using encoder/decoder architecture."""
 
-  def _create_modules(self, hidden_size):
-    encoder = Encoder.partial(hidden_size=hidden_size).shared(name='encoder')
-    decoder = Decoder.shared(name='decoder')
+  def _create_modules(self, hidden_size, num_layers, dropout):
+    encoder = Encoder.partial(hidden_size=hidden_size,
+                              num_layers=num_layers,
+                              horizontal_dropout_rate=dropout,
+                              vertical_dropout_rate=dropout).shared(name='encoder')
+    decoder = Decoder.partial(num_layers=num_layers,
+                              horizontal_dropout_rate=dropout,
+                              vertical_dropout_rate=dropout
+                              ).shared(name='decoder')
     return encoder, decoder
 
   def apply(self,
@@ -162,26 +274,28 @@ class Seq2seq(nn.Module):
             encoder_inputs_lengths: jnp.array,
             vocab_size: int,
             emb_dim: int = EMBEDDING_DIM,
-            teacher_force: bool = True,
-            hidden_size: int = LSTM_HIDDEN_SIZE):
+            train: bool = True,
+            hidden_size: int = LSTM_HIDDEN_SIZE,
+            num_layers = NUM_LAYERS,
+            dropout = DROPOUT):
     """Run the seq2seq model.
 
     Args:
       encoder_inputs: padded batch of input sequences to encode (as vocab 
       indices), shaped `[batch_size, question length]`.
-      decoder_inputs: padded batch of expected decoded sequences for teacher
-        forcing, shaped `[batch_size, query len]` on the train flow and shaped
+      decoder_inputs: padded batch of expected decoded sequences on the train
+        flow, shaped `[batch_size, query len]` on the train flow and shaped
         `[batch_size, max query len]` on the inference flow.
-        During inference (i.e., `teacher_force = False`), the initial time step
+        During inference (i.e., `train = False`), the initial time step
         is forced into the model and samples are used for the following inputs.
         The second dimension of this tensor determines how many steps will be
         decoded, regardless of the value of `teacher_force`.
       encoder_inputs_lengths: input sequences lengths [batch_size]
       vocab_size: size of vocabulary
       emb_dim: embedding dimension
-      teacher_force: bool, whether to use `decoder_inputs` as input to the
-        decoder at every step. If False, only the first input is used, followed
-        by samples taken from the previous output logits.
+      train: bool, differentiating between train and inference flow. This is
+        needed for the dropout on both the encoder and decoder, and on the
+        decoder it also activates teacher forcing (the gold sequence used).
       hidden_size: int, the number of hidden dimensions in the encoder and
         decoder LSTMs.
       Returns:
@@ -191,7 +305,7 @@ class Seq2seq(nn.Module):
         num_embeddings=vocab_size,
         features=emb_dim,
         embedding_init=nn.initializers.normal(stddev=1.0))
-    encoder, decoder = self._create_modules(hidden_size)
+    encoder, decoder = self._create_modules(hidden_size, num_layers, dropout)
 
     # compute attention masks
     mask = compute_attention_masks(encoder_inputs.shape, encoder_inputs_lengths)
@@ -199,7 +313,8 @@ class Seq2seq(nn.Module):
     # Encode inputs
     hidden_states, init_decoder_state = encoder(encoder_inputs,
                                                 encoder_inputs_lengths,
-                                                shared_embedding)
+                                                shared_embedding,
+                                                train)
     # Decode outputs.
     logits, predictions = decoder(init_decoder_state,
                                   hidden_states,
@@ -207,6 +322,6 @@ class Seq2seq(nn.Module):
                                   decoder_inputs[:, :-1],
                                   shared_embedding,
                                   vocab_size,
-                                  teacher_force=teacher_force)
+                                  train=train)
 
     return logits, predictions
