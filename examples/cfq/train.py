1a2192503f3722ff3e0e2bc0299d095689869234
ranapop <roxanappop7@gmail.com>
2021-02-11 19:33:40 +0200

    migrate train.py to linen

--- examples/cfq/train.py
+++ examples/cfq/train.py
@@ -27,18 +27,19 @@ import numpy as np
 import functools
 import jax
 import jax.numpy as jnp
+from jax import random
 from jax.experimental.optimizers import clip_grads
 
 import flax
 from flax import jax_utils
-from flax import nn
+from flax import linen as nn
 from flax.training import checkpoints
 from flax.training import common_utils
 from flax.metrics import tensorboard
 
 import input_pipeline as inp
 import input_pipeline_constants as inp_constants
-import models
+from models import Seq2seq
 
 BatchType = Dict[Text, jnp.array]
 
@@ -62,24 +63,38 @@ def mask_sequences(sequence_batch: jnp.array, lengths: jnp.array):
   mask = (lengths[:, jnp.newaxis] > jnp.arange(sequence_batch.shape[1]))
   return sequence_batch * mask
 
+#TODO Why do I need 'params' rng? 
+def make_rngs(rng: jax.random.PRNGKey, train: bool = False):
+  if train:
+    rng1, rng2, rng3 = random.split(rng, num=3)
+    rngs = {'params': rng1, 'dropout': rng2, 'lstm': rng3}
+    return rngs
+  else:
+    rng1, rng2 = random.split(rng)
+    return {'params': rng1, 'lstm': rng2}
 
-def create_model(vocab_size: int) -> nn.Module:
-  """Creates a seq2seq model."""
-  _, initial_params = models.Seq2seq.partial(
-      vocab_size=vocab_size
-  ).init_by_shape(
-      nn.make_rng(),
-      #encoder_inputs
-      [
-          ((1, 1), jnp.uint8),
+def get_initial_params(rng: jax.random.PRNGKey, vocab_size: int):
+  rngs = make_rngs(rng)
+  seq2seq = Seq2seq(vocab_size=vocab_size)
+  initial_batch = [
+          jnp.array((1, 1), jnp.uint8),
           #decoder_inputs
           # need to pass 2 for decoder length
           # as the first token is cut off
-          ((1, 2), jnp.uint8),
-          ((1,), jnp.uint8)
-      ])
-  model = nn.Model(models.Seq2seq, initial_params)
-  return model
+          jnp.array((1, 2), jnp.uint8),
+          jnp.array((1,), jnp.uint8)
+      ]
+  initial_batch = [
+      jnp.zeros((1, 1), jnp.uint8),
+      jnp.zeros((1, 2), jnp.uint8),
+      jnp.zeros((1,), jnp.uint8)
+    ]
+  initial_params = seq2seq.init(rngs,
+      initial_batch[0],
+      initial_batch[1],
+      initial_batch[2],
+      False)
+  return initial_params['params']
 
 
 def cross_entropy_loss(logits: jnp.array, labels: jnp.array,
@@ -201,8 +216,12 @@ def write_examples(file: TextIO, no_logged_examples: int,
 
 
 @functools.partial(jax.pmap, axis_name='batch', static_broadcasted_argnums=(3))
-def train_step(optimizer: Any, batch: BatchType, rng: Any, vocab_size: int):
+def train_step(optimizer: Any,
+    batch: BatchType,
+    rng: jax.random.PRNGKey,
+    vocab_size: int):
   """Train one step."""
+  step_rng = jax.random.fold_in(rng, optimizer.state.step)
 
   inputs = batch[inp_constants.QUESTION_KEY]
   input_lengths = batch[inp_constants.QUESTION_LEN_KEY]
@@ -210,13 +229,16 @@ def train_step(optimizer: Any, batch: BatchType, rng: Any, vocab_size: int):
   labels_no_bos = labels[:, 1:]
   queries_lengths = batch[inp_constants.QUERY_LEN_KEY] - 1
 
-  def loss_fn(model):
+  def loss_fn(params):
     """Compute cross-entropy loss."""
-    with nn.stochastic(rng):
-      logits, predictions, _ = model(encoder_inputs=inputs,
-                                     decoder_inputs=labels,
-                                     encoder_inputs_lengths=input_lengths,
-                                     vocab_size=vocab_size)
+    seq2seq = Seq2seq(vocab_size=vocab_size)
+    logits, predictions, _ = seq2seq.apply(
+      {'params': params}, 
+      encoder_inputs=inputs,
+      decoder_inputs=labels,
+      encoder_inputs_lengths=input_lengths,
+      train=True,
+      rngs=make_rngs(step_rng, True))
     loss = cross_entropy_loss(logits,
                               labels_no_bos,
                               queries_lengths,
@@ -238,15 +260,15 @@ def train_step(optimizer: Any, batch: BatchType, rng: Any, vocab_size: int):
   metrics = jax.lax.pmean(metrics, axis_name='batch')
   return optimizer, metrics
 
-
+#TODO: do I need a random key on the eval flow?
 @jax.partial(jax.jit, static_argnums=[4, 6])
-def infer(model: nn.Module, inputs: jnp.array, inputs_lengths: jnp.array,
-          rng: Any, vocab_size: int, bos_encoding: jnp.array,
+def infer(params: Dict, inputs: jnp.array, inputs_lengths: jnp.array,
+          rng: jax.random.PRNGKey, vocab_size: int, bos_encoding: jnp.array,
           predicted_output_length: int):
   """Apply model on inference flow and return predictions.
 
     Args:
-        model: the seq2seq model applied
+        model: the seq2seq params.
         inputs: batch of input sequences
         rng: rng
         vocab_size: size of vocabulary
@@ -259,12 +281,14 @@ def infer(model: nn.Module, inputs: jnp.array, inputs_lengths: jnp.array,
   # is the desired output length + 2 (bos and eos tokens).
   initial_dec_inputs = jnp.tile(bos_encoding,
                                 (inputs.shape[0], predicted_output_length + 2))
-  with nn.stochastic(rng):
-    logits, predictions, attention_weights = model(encoder_inputs=inputs,
-      decoder_inputs=initial_dec_inputs,
-      encoder_inputs_lengths=inputs_lengths,
-      vocab_size=vocab_size,
-      train=False)
+  seq2seq = Seq2seq(vocab_size=vocab_size)
+  logits, predictions, attention_weights = seq2seq.apply(
+    {'params': params},
+    encoder_inputs=inputs,
+    decoder_inputs=initial_dec_inputs,
+    encoder_inputs_lengths=inputs_lengths,
+    train=False,
+    rngs=make_rngs(rng, False))
   return logits, predictions, attention_weights
 
 
@@ -293,8 +317,9 @@ def evaluate_model(model: nn.Module,
     inputs = batch[inp_constants.QUESTION_KEY]
     input_lengths = batch[inp_constants.QUESTION_LEN_KEY]
     gold_outputs = batch[inp_constants.QUERY_KEY][:, 1:]
+    #TODO: do not use rngs on eval.
     logits, inferred_outputs, attention_weights = infer(model,
-                                inputs, input_lengths, nn.make_rng(),
+                                inputs, input_lengths, random.PRNGKey(0),
                                 data_source.tokens_vocab_size,
                                 data_source.bos_idx,
                                 predicted_output_length)
@@ -356,59 +381,60 @@ def train_model(learning_rate: float = None,
         os.path.join(model_dir, 'train'))
     eval_summary_writer = tensorboard.SummaryWriter(
         os.path.join(model_dir, 'eval'))
+  
+  rng = jax.random.PRNGKey(seed)
+  rng, init_rng = jax.random.split(rng)
+  initial_params = get_initial_params(init_rng, data_source.tokens_vocab_size)
+  optimizer = flax.optim.Adam(learning_rate=learning_rate).create(initial_params)
+  optimizer = jax_utils.replicate(optimizer)
 
-  with nn.stochastic(jax.random.PRNGKey(seed)):
-    model = create_model(data_source.tokens_vocab_size)
-    optimizer = flax.optim.Adam(learning_rate=learning_rate).create(model)
-    optimizer = jax_utils.replicate(optimizer)
+  if bucketing:
+    train_batches = data_source.get_bucketed_batches(
+        split = 'train',
+        batch_size = batch_size,
+        bucket_size = 8,
+        drop_remainder = True,
+        shuffle = True)
+  else:
+    train_batches = data_source.get_batches(split = 'train',
+                                            batch_size = batch_size,
+                                            shuffle = True,
+                                            drop_remainder=True)
 
-    if bucketing:
-      train_batches = data_source.get_bucketed_batches(
-          split = 'train',
-          batch_size = batch_size,
-          bucket_size = 8,
-          drop_remainder = True,
-          shuffle = True)
-    else:
-      train_batches = data_source.get_batches(split = 'train',
-                                              batch_size = batch_size,
-                                              shuffle = True,
-                                              drop_remainder=True)
+  dev_batches = data_source.get_batches(split = 'dev',
+                                        batch_size = batch_size,
+                                        shuffle = True,
+                                        drop_remainder = True)
 
-    dev_batches = data_source.get_batches(split = 'dev',
-                                          batch_size = batch_size,
-                                          shuffle = True,
-                                          drop_remainder = True)
+  train_iter = iter(train_batches)
+  train_metrics = []
+  for step, batch in zip(range(num_train_steps), train_iter):
+    if batch_size % jax.device_count() > 0:
+      raise ValueError('Batch size must be divisible by the number of devices')
+    batch = common_utils.shard(jax.tree_map(lambda x: x._numpy(), batch))
+    rng, step_key = jax.random.split(rng)
+    # Shard the step PRNG key
+    sharded_keys = common_utils.shard_prng_key(step_key)
+    optimizer, metrics = train_step(optimizer, batch, sharded_keys,
+                                    data_source.tokens_vocab_size)
+    train_metrics.append(metrics)
+    if (step + 1) % eval_freq == 0:
+      train_metrics = common_utils.get_metrics(train_metrics)
+      train_summary = jax.tree_map(lambda x: x.mean(), train_metrics)
+      train_metrics = []
+      # evaluate
+      model = jax_utils.unreplicate(optimizer.target)  # Fetch from 1st device
+      dev_metrics = evaluate_model(model=model,
+                                  batches=dev_batches,
+                                  data_source=data_source,
+                                  predicted_output_length=max_out_len,
+                                  logging_file_name = logging_file_name,
+                                  no_logged_examples=3)
+      log(step, train_summary, dev_metrics)
+      save_to_tensorboard(train_summary_writer, train_summary, step)
+      save_to_tensorboard(eval_summary_writer, dev_metrics, step)
 
-    train_iter = iter(train_batches)
-    train_metrics = []
-    for step, batch in zip(range(num_train_steps), train_iter):
-      if batch_size % jax.device_count() > 0:
-        raise ValueError('Batch size must be divisible by the number of devices')
-      batch = common_utils.shard(jax.tree_map(lambda x: x._numpy(), batch))
-      step_key = nn.make_rng()
-      # Shard the step PRNG key
-      sharded_keys = common_utils.shard_prng_key(step_key)
-      optimizer, metrics = train_step(optimizer, batch, sharded_keys,
-                                      data_source.tokens_vocab_size)
-      train_metrics.append(metrics)
-      if (step + 1) % eval_freq == 0:
-        train_metrics = common_utils.get_metrics(train_metrics)
-        train_summary = jax.tree_map(lambda x: x.mean(), train_metrics)
-        train_metrics = []
-        # evaluate
-        model = jax_utils.unreplicate(optimizer.target)  # Fetch from 1st device
-        dev_metrics = evaluate_model(model=model,
-                                    batches=dev_batches,
-                                    data_source=data_source,
-                                    predicted_output_length=max_out_len,
-                                    logging_file_name = logging_file_name,
-                                    no_logged_examples=3)
-        log(step, train_summary, dev_metrics)
-        save_to_tensorboard(train_summary_writer, train_summary, step)
-        save_to_tensorboard(eval_summary_writer, dev_metrics, step)
-
-    logging.info('Done training')
+  logging.info('Done training')
 
   logging.info('Saving model at %s', model_dir)
   checkpoints.save_checkpoint(model_dir, optimizer, num_train_steps)
@@ -420,20 +446,20 @@ def train_model(learning_rate: float = None,
 def test_model(model_dir, data_source: inp.CFQDataSource, max_out_len: int,
                seed: int, batch_size: int):
   """Evaluate model at model_dir on dev subset"""
-  with nn.stochastic(jax.random.PRNGKey(seed)):
-    logging_file_name = os.path.join(model_dir, 'eval_logged_examples.txt')
-    model = create_model(data_source.tokens_vocab_size)
-    optimizer = flax.optim.Adam().create(model)
-    optimizer = checkpoints.restore_checkpoint(model_dir, optimizer)
-    dev_batches = data_source.get_batches(split = 'dev',
-                                          batch_size=batch_size,
-                                          shuffle=True)
-    # evaluate
-    dev_metrics = evaluate_model(model=optimizer.target,
-                                 batches=dev_batches,
-                                 data_source=data_source,
-                                 predicted_output_length=max_out_len,
-                                 logging_file_name = logging_file_name,
-                                 no_logged_examples=3)
-    logging.info('Loss %.4f, acc %.2f', dev_metrics[LOSS_KEY],
-                 dev_metrics[ACC_KEY])
+  rng = jax.random.PRNGKey(seed)
+  logging_file_name = os.path.join(model_dir, 'eval_logged_examples.txt')
+  model = create_model(data_source.tokens_vocab_size)
+  optimizer = flax.optim.Adam().create(model)
+  optimizer = checkpoints.restore_checkpoint(model_dir, optimizer)
+  dev_batches = data_source.get_batches(split = 'dev',
+                                        batch_size=batch_size,
+                                        shuffle=True)
+  # evaluate
+  dev_metrics = evaluate_model(model=optimizer.target,
+                                batches=dev_batches,
+                                data_source=data_source,
+                                predicted_output_length=max_out_len,
+                                logging_file_name = logging_file_name,
+                                no_logged_examples=3)
+  logging.info('Loss %.4f, acc %.2f', dev_metrics[LOSS_KEY],
+                dev_metrics[ACC_KEY])
